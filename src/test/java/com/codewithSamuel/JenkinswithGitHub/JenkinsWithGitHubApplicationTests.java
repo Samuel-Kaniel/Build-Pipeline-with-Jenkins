b6d1b2f65852aa0ba2e6668f4f8fd292d1e4b503
SAMUEL ABREHA <83005750+Samuel-Kaniel@users.noreply.github.com>
2023-10-03 19:21:23 -0700

    Update JenkinsWithGitHubApplicationTests.java

--- src/test/java/com/codewithSamuel/JenkinswithGitHub/JenkinsWithGitHubApplicationTests.java
+++ src/test/java/com/codewithSamuel/JenkinswithGitHub/JenkinsWithGitHubApplicationTests.java
@@ -1,6 +1,6 @@
 package com.codewithSamuel.JenkinswithGitHub;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
